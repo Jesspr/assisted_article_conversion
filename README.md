5891d0a3dfda59ccb8abed238e4a064a8a76a0ac
jesspr <jespreuss@gmail.com>
2023-04-01 12:20:13 +0200

    provided commented version and readme file

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# assisted_article_conversion
+# Google Analytics Data Analysis of Assisted Article Conversion
 
-I query the Google BigQuery database from the Universal Analytics data for the number of items that helped complete an order. For this, the orders of the previous day are taken. For these orders, the transaction ID is used to see which pages the user has visited before. The last 30 days and the last three pages are included. The pages that were not articles (e.g. homepage) are then excluded. 
+This project is an analysis of Google Analytics data I did while working with a publishing house, performed using SQL in Google BigQuery. The goal of the analysis is to identify which pages users visited before making a purchase and how these pages contributed to the purchase, i.e., whether they were the last click before the purchase or an assisted click.
+
+## Data Source
+The data is sourced from Google Analytics Universal Analytics data for the previous day. Specifically, the transaction ID is used to see which pages the user has visited before, including the last 30 days and the last three pages. The pages that were not articles (e.g., homepage) are then excluded.
+
+## Analysis Steps
+The analysis consists of the following steps:
+
+    *Extracting Purchase Data:* Extract data for all purchases made in the previous day, including the transaction ID, the page where the purchase was made, and the ID of the article that was purchased.
+
+    *Identifying Last Three Page Visits:* For each user who made a purchase, identify the last three pages they visited before the purchase, excluding pages that were not articles. This is done using the LEAD function to identify the second and third pages, and then filtering to only include visits where all three pages were articles.
+
+    *Matching Page Visits to Purchases:* Match each purchase to the user's last three page visits, and identify whether the purchase was made on the last click or as an assisted click. An assisted click is defined as a click on a page that the user visited before the last click, within the last 30 days. This is done by joining the purchase data with the page visit data and comparing the hit numbers and timestamps.
+
+    *Aggregating Results:* Aggregate the results by date and assisted page ID, and sum the number of assisted transactions. If a page was visited multiple times before a purchase, it will appear as an assisted click for each of those visits.
+
+## Results
+The results of the analysis are presented in a table that shows, for each date, the ID of the assisted page and the number of assisted transactions for that page. The table is sorted by the number of assisted transactions, with the most important assisted pages at the top. The results can be used to optimize the publishing house's website and marketing strategy, by identifying which pages are most effective in driving purchases and which pages could be improved to increase their impact.
+
+## Conclusion
+This project demonstrates how SQL can be used to extract valuable insights from Google Analytics data. The code is provided as an open-source project on GitHub, to allow others to replicate the analysis or use it as a starting point for their own data analysis projects.
--- assisted_conversions.sql
+++ assisted_conversions.sql
@@ -1,114 +1,105 @@
+-- Define a CTE called 'purchases' that contains information about ecommerce purchases.
 WITH purchases
 AS (
-SELECT
-      PARSE_DATE("%Y%m%d", date) AS date,
-      MAX(TRANSACTION.transactionid) AS transactionid,
-      h1.hitNumber AS purchaseHit,
-      h1.time AS purchaseHitStartTime,  
-      clientId,
-    REGEXP_EXTRACT(REGEXP_REPLACE(h1.page.pagePath,"/amp",""),r"-id-([0-9]{5,10})$") as article_id
-      h1.page.pagePath AS page,
-      COUNT (*) AS orders
-    FROM `project.dataset.table_*`, UNNEST (hits) AS h1
-    WHERE h1.eCommerceAction.action_type = '6'
-    AND REGEXP_CONTAINS(h1.page.pagePath, r".*-id-[0-9].*")
-    AND transaction.transactionid IS NOT NULL
-
-      AND _table_suffix = FORMAT_DATE('%Y%m%d',DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY))
-    GROUP BY date ,article_id,page,purchaseHit,purchaseHitStartTime,clientId
-    ),
-    
-    
-   last_hits AS (
-    SELECT
-  pagePath,
-  clientId,
-  hitNumber,
-  hitTime,
-  second_pagePath,
-  third_pagePath,
-   REGEXP_EXTRACT(REGEXP_REPLACE(pagePath,"/amp",""),r"-id-([0-9]{5,10})$") as first_article_id,
-   REGEXP_EXTRACT(REGEXP_REPLACE(second_pagePath,"/amp",""),r"-id-([0-9]{5,10})$") as second_article_id,
-   REGEXP_EXTRACT(REGEXP_REPLACE(third_pagePath,"/amp",""),r"-id-([0-9]{5,10})$") as third_article_id,
-  upcomingtransactionid,
-  COUNT(*) AS count
-FROM (
-
   SELECT
-        clientId,
-        h1.hitNumber AS hitNumber,
-        h1.time AS hitTime,
-        h1.page.pagePath AS pagePath,
-        LEAD (h1.page.pagePath, 1) OVER (PARTITION BY clientId ORDER BY h1.time) AS second_pagePath,
-        LEAD (h1.page.pagePath, 2) OVER (PARTITION BY clientId ORDER BY h1.time) AS third_pagePath,
-        LAST_VALUE((SELECT MAX(transaction.transactionid) 
-              FROM UNNEST(hits) AS h1 WHERE transaction.transactionid IS NOT NULL) IGNORE NULLS) OVER (PARTITION BY fullvisitorid ORDER BY h1.time                  DESC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS upcomingtransactionid,
-    FROM `project.dataset.table_*`, UNNEST (hits) AS h1
-      WHERE _table_suffix BETWEEN FORMAT_DATE('%Y%m%d',DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)) AND FORMAT_DATE('%Y%m%d',DATE_SUB(CURRENT_DATE(),         INTERVAL 1 DAY))
-      AND  h1.type = "PAGE"
- ORDER BY
+    PARSE_DATE("%Y%m%d", date) AS date, -- Convert the date string to a date type.
+    MAX(TRANSACTION.transactionid) AS transactionid, -- Get the maximum transaction ID for the hit
+    h1.hitNumber AS purchaseHit,
+    h1.time AS purchaseHitStartTime,  
+    clientId,
+    -- Extract the article ID from the page path using a regular expression.
+    REGEXP_EXTRACT(REGEXP_REPLACE(h1.page.pagePath,"/amp",""),r"-id-([0-9]{5,10})$") as article_id
+    h1.page.pagePath AS page,
+    COUNT (*) AS orders
+  FROM `project.dataset.table_*`, UNNEST (hits) AS h1
+  WHERE h1.eCommerceAction.action_type = '6' -- Filters only ecommerce purchase 
+    AND REGEXP_CONTAINS(h1.page.pagePath, r".*-id-[0-9].*") -- Filters pages that contain article IDs
+    AND transaction.transactionid IS NOT NULL -- Filters transactions that have a transaction ID
+    AND _table_suffix = FORMAT_DATE('%Y%m%d',DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)) -- Only include data from yesterday's tables
+  GROUP BY date, article_id, page, purchaseHit, purchaseHitStartTime, clientId
+),
+    
+-- Define a CTE called 'last_hits' that contains information about the last three pages viewed before a purchase.    
+last_hits AS (
+  SELECT
+    pagePath,
+    clientId,
+    hitNumber,
+    hitTime,
+    second_pagePath,
+    third_pagePath,
+    -- Extract the article ID from the page path using a regular expression.
+    REGEXP_EXTRACT(REGEXP_REPLACE(pagePath,"/amp",""),r"-id-([0-9]{5,10})$") as first_article_id,
+    REGEXP_EXTRACT(REGEXP_REPLACE(second_pagePath,"/amp",""),r"-id-([0-9]{5,10})$") as second_article_id,
+    REGEXP_EXTRACT(REGEXP_REPLACE(third_pagePath,"/amp",""),r"-id-([0-9]{5,10})$") as third_article_id,
+    upcomingtransactionid,
+    COUNT(*) AS count
+  FROM (
+    SELECT
       clientId,
-      hitTime)
+      h1.hitNumber AS hitNumber,
+      h1.time AS hitTime,
+      h1.page.pagePath AS pagePath,
+      LEAD (h1.page.pagePath, 1) OVER (PARTITION BY clientId ORDER BY h1.time) AS second_pagePath,
+      LEAD (h1.page.pagePath, 2) OVER (PARTITION BY clientId ORDER BY h1.time) AS third_pagePath,
+      LAST_VALUE(
+        (
+          SELECT MAX(transaction.transactionid) 
+          FROM UNNEST(hits) AS h1 
+          WHERE transaction.transactionid IS NOT NULL
+        ) IGNORE NULLS
+      ) OVER (PARTITION BY fullvisitorid ORDER BY h1.time DESC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS upcomingtransactionid,
+    FROM `project.dataset.table_*`, UNNEST (hits) AS h1
+      WHERE _table_suffix BETWEEN FORMAT_DATE('%Y%m%d',DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)) AND FORMAT_DATE('%Y%m%d',DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)) -- Only include data from the past 30 days (up to yesterday's data)
+        AND h1.type = "PAGE" -- Filters only pageview events
+      ORDER BY clientId, hitTime
+  )
  
-WHERE
-  third_pagePath LIKE "%-id-%"
+WHERE third_pagePath LIKE "%-id-%" -- Filters only pages that contain an article ID
   AND second_pagePath LIKE "%-id-%"
   AND pagePath LIKE "%-id-%"
 
-GROUP BY
-  pagePath,
-  second_pagePath,
-  third_pagePath,
-    first_article_id,
- second_article_id,
- third_article_id,
-  clientId,
-  upcomingtransactionid,
-  hitTime,
-  hitNumber
+GROUP BY pagePath, second_pagePath, third_pagePath, first_article_id, second_article_id, third_article_id, clientId, upcomingtransactionid, hitTime, hitNumber
   HAVING (third_article_id != first_article_id OR third_article_id != second_article_id)
 
 ORDER BY
   count DESC
  ),
     
-    
-  list AS (  
-SELECT
+  -- Define the CTE named "list". This subquery retrieves data from two tables "purchases" and "last_hits" and joins them based on the "clientId" column.    
+list AS (  
+  SELECT
     date,
-  first_article_id,
- second_article_id,
- third_article_id,
-CASE WHEN purchaseHit = hitNumber THEN "last click" ELSE "assisted click" END AS conversionType,
-COUNT (DISTINCT transactionid) AS Transactions
-FROM 
+    first_article_id,
+    second_article_id,
+    third_article_id,
+    CASE WHEN purchaseHit = hitNumber THEN "last click" ELSE "assisted click" END AS conversionType, -- identifies the type of conversion: last click or assisted click
+    COUNT (DISTINCT transactionid) AS Transactions -- count of transactions for a specific combination of the article IDs and conversion type
+  FROM 
     (
     SELECT *
     FROM `purchases`
-     ) AS a
+    ) AS a  -- subquery to retrieve all data from the purchases table
     LEFT JOIN 
     (SELECT *
-    FROM `last_hits`) AS b
+    FROM `last_hits`) AS b -- subquery to retrieve all data from the last_hits table
     
     USING (clientId)
-  WHERE
-    upcomingtransactionid = transactionid
-    AND purchaseHit >= hitNumber
-    AND page = third_pagePath
-    AND purchaseHitStartTime - hitTime <= (30*24*60*60)
+    WHERE upcomingtransactionid = transactionid -- selecting only matching transactions
+      AND purchaseHit >= hitNumber 
+      AND page = third_pagePath
+      AND purchaseHitStartTime - hitTime <= (30*24*60*60) -- filter transactions that occurred within the last 30 days
     GROUP BY date,page,pagePath,second_pagePath,third_pagePath,first_article_id, second_article_id, third_article_id,conversionType
     HAVING conversionType = 'assisted click'
 )
-
+-- Select the date, assisted article ID, and sum of transactions for the combination of date and assisted article ID from the CTE named "list".
 SELECT 
-date,
-CASE 
-WHEN third_article_id != first_article_id THEN first_article_id
-WHEN third_article_id != second_article_id THEN second_article_id
-END AS assisted_id,
-SUM(Transactions) AS assisted_transactions
-
-
+  date,
+  CASE 
+  WHEN third_article_id != first_article_id THEN first_article_id
+  WHEN third_article_id != second_article_id THEN second_article_id
+  END AS assisted_id,
+  SUM(Transactions) AS assisted_transactions
 FROM `list`
 GROUP BY date,assisted_id
 ORDER BY assisted_transactions
